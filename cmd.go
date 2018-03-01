eb204e1233e61b86df1ec9184af5b8837b7df37e
Daniel Schulz <daniel.schulz@ocono.de>
2018-03-01 19:35:46 +0100

    Implement data parsing

--- cmd.go
+++ cmd.go
@@ -6,7 +6,17 @@ import (
 	"bufio"
 	"io/ioutil"
 	"strings"
+	"strconv"
+	"fmt"
 )
+
+type Simulation struct {
+	Rows, Columns, Vehicles, Rides, Bonus, Steps int
+}
+
+type Ride struct {
+	StartX, StartY, EndX, EndY, Earliest, Latest int
+}
 
 func main() {
 	var inputPath string
@@ -26,15 +36,68 @@ func main() {
 	data := []string{}
 
 	scanner := bufio.NewScanner(file)
+
+	scanner.Scan()
+	firstLine := scanner.Text()
+	simulation := createSimulation(firstLine)
+
+	fmt.Println(simulation)
+
+	rides := []Ride{}
+
 	for scanner.Scan() {
 		currentLine := scanner.Text()
 
-		data = append(data, currentLine)
+		rides = append(rides, createRide(currentLine))
 	}
+
+	fmt.Println(rides)
 
 	if err := scanner.Err(); err != nil {
 		panic(err)
 	}
 
 	ioutil.WriteFile(outPath, []byte(strings.Join(data, ",")), 0644)
+}
+
+func createSimulation(line string) Simulation {
+	intSlice := toIntSlice(line)
+
+	return Simulation{
+		intSlice[0],
+		intSlice[1],
+		intSlice[2],
+		intSlice[3],
+		intSlice[4],
+		intSlice[5],
+	}
+}
+
+func createRide(line string) Ride {
+	intSlice := toIntSlice(line)
+
+	return Ride{
+		intSlice[0],
+		intSlice[1],
+		intSlice[2],
+		intSlice[3],
+		intSlice[4],
+		intSlice[5],
+	}
+}
+
+func toIntSlice(s string) []int {
+	parts := strings.Split(s, " ")
+	intSlice := make([]int, len(parts))
+
+	for i, firstLinePart := range parts {
+		firstLineInt, err := strconv.Atoi(firstLinePart)
+		if err != nil {
+			panic(err)
+		}
+
+		intSlice[i] = firstLineInt
+	}
+
+	return intSlice
 }
